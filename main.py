3cf550f56a1739c74cd61525c4871f7c0fc8ee86
Ayman Cassim <aycas2004@gmail.com>
2024-10-07 18:33:33 +0800

    Changed from using admin file to admin_2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from login import checkLogin
-from admin import adminFunctions
+# from admin import adminFunctions
 from manager import managerFunctions
 from customer import customerFunctions
 from chef import chef_menu
+
+from admin_2 import adminFunctions
 
 from styles import *
 
@@ -14,7 +16,7 @@ if loginStatus["response"]:
 	elif loginStatus["role"] == "Manager":
 		managerFunctions(loginStatus["email"])
 	elif loginStatus["role"] == "Chef":
-		chef_menu(loginStatus["email"])
+		chef_menu()
 	elif loginStatus["role"] == "Customer":
 		customerFunctions(loginStatus["email"])
 	else:
